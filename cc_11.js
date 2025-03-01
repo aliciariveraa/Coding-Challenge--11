7687b9c25ee1c29f323eddf3da598f1ec490bcde
aliciariveraa <aliciarivera@usf.edu>
2025-03-01 00:29:46 -0500

    "Task 2 - Created Borrower Class"

--- cc_11.js
+++ cc_11.js
@@ -24,3 +24,35 @@ const book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 123456, 5);
 console.log(book1.getDetails()); // Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 5"
 book1.updateCopies(-1);
 console.log(book1.getDetails()); // Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 4"
+
+
+
+// Task 2 - Created Borrower Class
+
+class Borrower {
+    constructor(name, borrowerId) {
+        this.name = name;
+        this.borrowerId = borrowerId;
+        this.borrowedBooks = [];
+    }
+
+    // Method to borrow a book
+    borrowBook(book) {
+        this.borrowedBooks.push(book);
+    }
+
+    // Method to return a book
+    returnBook(book) {
+        const index = this.borrowedBooks.indexOf(book);
+        if (index !== -1) {
+            this.borrowedBooks.splice(index, 1);
+        }
+    }
+}
+
+// Testing Task 2
+const borrower1 = new Borrower("Alice Johnson", 201);
+borrower1.borrowBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks); // Expected output: ["The Great Gatsby"]
+borrower1.returnBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks); // Expected output: []
